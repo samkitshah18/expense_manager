ad7d00a761bdf21e8239802e6f47b31c62782b2f
samkitshah18 <samkitvshah18@gmail.com>
2020-08-09 22:00:24 +0530

    Initial commit

---
+++ lib/widgets/user_transactions.dart
@@ -0,0 +1,69 @@
+// import '../models/transaction.dart';
+
+// import './new_transaction.dart';
+// import './transaction_list.dart';
+// import 'package:flutter/material.dart';
+
+// class UserTransactions extends StatefulWidget {
+//   @override
+//   _UserTransactionsState createState() => _UserTransactionsState();
+// }
+
+// class _UserTransactionsState extends State<UserTransactions> {
+ 
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       children: <Widget>[
+//         NewTransaction(_addNewTransaction),
+//         TransactionList(_userTransaction),
+//       ],
+//     );
+//   }
+// }
+
+
+// return Card(
+//                   child: Row(
+//                     children: <Widget>[
+//                       Container(
+//                         margin: EdgeInsets.symmetric(
+//                           vertical: 10,
+//                           horizontal: 15,
+//                         ),
+//                         decoration: BoxDecoration(
+//                           border: Border.all(
+//                             color: Theme.of(context).primaryColor,
+//                             width: 2,
+//                           ),
+//                         ),
+//                         padding: EdgeInsets.all(10),
+//                         child: Text(
+//                           '₹ ${transactions[index].amount.toStringAsFixed(2)}',
+//                           style: TextStyle(
+//                             fontWeight: FontWeight.bold,
+//                             fontSize: 20,
+//                             color: Theme.of(context).primaryColor,
+//                           ),
+//                         ),
+//                       ),
+//                       Column(
+//                         crossAxisAlignment: CrossAxisAlignment.start,
+//                         children: <Widget>[
+//                           Text(
+//                             transactions[index].title,
+//                             style: Theme.of(context).textTheme.title,
+//                           ),
+//                           Text(
+//                             DateFormat.yMMMd().format(transactions[index].date),
+//                             style: TextStyle(
+//                               color: Colors.grey,
+//                             ),
+//                           )
+//                         ],
+//                       ),
+//                     ],
+//                   ),
+//                 );
